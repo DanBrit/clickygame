d4138faf1e7195d78834372ceb338c8fa88d086f
dmar <dan.britain.marshall@gmail.com>
2019-04-27 12:10:28 -0500

    Updates

---
+++ precache-manifest.d88017c25278ad14454b4d4c240aa294.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da87c1bc01a8a36be484ab94ac556705",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "6b683862488881ae1af2",
+    "url": "/clickygame/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "bf94639fbfd0c4ed54c1",
+    "url": "/clickygame/static/css/main.3a45a15b.chunk.css"
+  },
+  {
+    "revision": "6b683862488881ae1af2",
+    "url": "/clickygame/static/js/2.2c795f6e.chunk.js"
+  },
+  {
+    "revision": "bf94639fbfd0c4ed54c1",
+    "url": "/clickygame/static/js/main.30defbef.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
